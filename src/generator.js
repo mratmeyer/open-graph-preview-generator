c2263e618177b886690695750416b44226487d5f
mratmeyer <17440617+mratmeyer@users.noreply.github.com>
2021-05-12 16:36:19 -0400

    Separate canvas generator into separate file

---
+++ src/generator.js
@@ -0,0 +1,48 @@
+const utils = require('./utils.js');
+const path = require('path');
+
+const { createCanvas, loadImage } = require('canvas');
+
+async function generateCard(title, date, tags) {
+    const canvas = createCanvas(1280, 704)
+    const ctx = canvas.getContext('2d')
+
+    // ctx.fillStyle = "white";
+    ctx.fillStyle = "#191919";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const image = await loadImage(path.resolve('assets/profile-256x256.png'));
+    ctx.drawImage(image, 50, 50, 178, 178);
+
+    ctx.font = '100px InterBold';
+    // ctx.fillStyle = "black";
+    ctx.fillStyle = "#E1E1E1";
+    ctx.fillText(utils.formatText(ctx, title), 50, 350);
+
+    ctx.font = '65px InterBold';
+    ctx.fillText("Max Ratmeyer's Blog", 275, 165);
+
+    ctx.font = '40px InterBold';
+    // ctx.fillStyle = "#414A4C";
+    ctx.fillStyle = "#E1E1E1";
+    ctx.fillText(date, 50, 664);
+
+    let formattedTags = 'Tagged: ';
+    for(i in tags) {
+        let tag = tags[i].charAt(0).toUpperCase() + tags[i].slice(1);
+        if (i < tags.length - 1) {
+            formattedTags = formattedTags + tag + ", ";
+        } else {
+            formattedTags = formattedTags + tag;
+        }
+    }
+
+    ctx.font = '40px InterBold';
+    // ctx.fillStyle = "#414A4C";
+    ctx.fillStyle = "#E1E1E1";
+    ctx.fillText(formattedTags, 600, 664);
+
+    return canvas.toBuffer('image/png');
+}
+
+module.exports = { generateCard };
